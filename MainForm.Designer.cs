75bee5e5f2908b15f9a88b41bc01fdf9ee905aab
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-03-07 23:45:13 +0200

    First labwork

---
+++ MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ComputerGraphics
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label = new System.Windows.Forms.Label();
+            this.draw = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Calibri Light", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label.Location = new System.Drawing.Point(113, 9);
+            this.label.MaximumSize = new System.Drawing.Size(270, 30);
+            this.label.MinimumSize = new System.Drawing.Size(270, 30);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(270, 30);
+            this.label.TabIndex = 0;
+            this.label.Text = "Лабораторна робота № 1 ";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // draw
+            // 
+            this.draw.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.draw.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.draw.Location = new System.Drawing.Point(149, 113);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(80, 50);
+            this.draw.TabIndex = 1;
+            this.draw.Text = "Draw";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.DrawSurname_Click);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 178);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(496, 247);
+            this.panel.TabIndex = 2;
+            // 
+            // close
+            // 
+            this.close.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.Location = new System.Drawing.Point(262, 113);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(80, 50);
+            this.close.TabIndex = 3;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(113, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Виконав студент групи ІП-14 ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri Light", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(110, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Бабіч Денис Володимирович ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(36)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(520, 437);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.draw);
+            this.Controls.Add(this.label);
+            this.Font = new System.Drawing.Font("Calibri Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Лабораторна робота № 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ComputerGraphics
+{
+    public partial class MainForm : Form
+    {
+        private const float THICKNESS = 5.0f;
+        private readonly Pen PEN = new Pen(Color.GhostWhite, THICKNESS);
+        private readonly Brush BRUSH = new SolidBrush(Color.GhostWhite);
+        private Graphics graphics;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            graphics = panel.CreateGraphics();
+        }
+
+        private void DrawSurname_Click(object sender, EventArgs e)
+        {
+            //Б
+            graphics.DrawLine(PEN, 5, 5, 5, panel.Height);
+            graphics.DrawLine(PEN, 5, 5, 100, 5);
+            graphics.DrawArc(PEN, -55, 70, 150, 175, 100.0F, -200.0f);
+
+            //a
+            graphics.DrawArc(PEN, 130, 90, 75, 150, 45.0f, 270.0f);
+            graphics.DrawLine(PEN, 201, 70, 201, panel.Height);
+
+            //б
+            graphics.DrawArc(PEN, 209, 100, 100, 145, 100.0f, -200.0f);
+            graphics.DrawLine(PEN, 245, 80, 245, panel.Height);
+            graphics.DrawCurve(PEN, new Point[] { new Point(244, 80), new Point(260, 70), new Point(290, 70), new Point(300, 60) });
+
+            //i
+            graphics.DrawLine(PEN, 360, 90, 360, panel.Height);
+            graphics.FillEllipse(BRUSH, 350, 30, 18, 18);
+
+            //ч
+            graphics.DrawArc(PEN, 410, 50, 80, 100, -10.0f, 200.0f);
+            graphics.DrawLine(PEN, 490, 100, 490, panel.Height);
+        }
+
+        private void Close_Click(object sender, EventArgs e) => this.Close();
+    }
+}
