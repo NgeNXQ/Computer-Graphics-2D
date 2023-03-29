1ca587760168f0ebb0fc74e32e1e01521babcc11
Denys Babich <89969565+NgeNXQ@users.noreply.github.com>
2023-03-29 19:00:17 +0300

    Labwork 3

---
+++ MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ThirdLabwork
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel = new System.Windows.Forms.Panel();
+            this.NumberInputBar = new System.Windows.Forms.NumericUpDown();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberInputBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(600, 600);
+            this.panel.TabIndex = 1;
+            // 
+            // NumberInputBar
+            // 
+            this.NumberInputBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.NumberInputBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NumberInputBar.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NumberInputBar.ForeColor = System.Drawing.Color.White;
+            this.NumberInputBar.Location = new System.Drawing.Point(12, 641);
+            this.NumberInputBar.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.NumberInputBar.Name = "NumberInputBar";
+            this.NumberInputBar.Size = new System.Drawing.Size(200, 33);
+            this.NumberInputBar.TabIndex = 2;
+            this.NumberInputBar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.DrawButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.DrawButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.DrawButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DrawButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DrawButton.ForeColor = System.Drawing.Color.White;
+            this.DrawButton.Location = new System.Drawing.Point(478, 624);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(64, 64);
+            this.DrawButton.TabIndex = 3;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.ClearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearButton.Image = ((System.Drawing.Image)(resources.GetObject("ClearButton.Image")));
+            this.ClearButton.Location = new System.Drawing.Point(548, 624);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(64, 64);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(625, 700);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.DrawButton);
+            this.Controls.Add(this.NumberInputBar);
+            this.Controls.Add(this.panel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(641, 739);
+            this.MinimumSize = new System.Drawing.Size(641, 739);
+            this.Name = "MainForm";
+            this.Text = "Лабораторна робота № 3";
+            ((System.ComponentModel.ISupportInitialize)(this.NumberInputBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.NumericUpDown NumberInputBar;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ThirdLabwork
+{
+    public partial class MainForm : Form
+    {
+        private readonly Graphics graphics;
+        private const float PEN_WIDTH = 1f;
+        private const float SIDE_LENGTH = 300.0f;
+        private const float BOTTOM_OFFSET = 75.0f;
+        private readonly Pen whitePen;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            graphics = panel.CreateGraphics();
+            whitePen = new Pen(Color.White, PEN_WIDTH);
+        }
+
+        private void DrawButton_Click(object sender, System.EventArgs e)
+        {
+            float height = (float)Math.Sqrt(3.0f) / 2.0f;
+            float halfSide = SIDE_LENGTH / 2.0f;
+            PointF panelCenter = new PointF(panel.Width / 2.0f, panel.Height / 2.0f);
+
+            PointF point1 = new PointF(panelCenter.X - halfSide, panelCenter.Y + halfSide * height - BOTTOM_OFFSET);
+            PointF point2 = new PointF(panelCenter.X + halfSide, panelCenter.Y + halfSide * height - BOTTOM_OFFSET);
+            PointF point3 = new PointF(panelCenter.X, panelCenter.Y - halfSide * height - BOTTOM_OFFSET);
+
+            DrawKochSnowflake(graphics, whitePen, point1, point2, point3, (int)NumberInputBar.Value);
+        }
+
+        private void ClearButton_Click(object sender, System.EventArgs e) => graphics.Clear(this.BackColor);
+
+        private void DrawKochSnowflake(Graphics graphics, Pen pen, PointF point1, PointF point2, PointF point3, int depth)
+        {
+            graphics.Clear(this.BackColor);
+
+            if (depth == 0)
+            {
+                graphics.DrawLine(pen, point1, point2);
+                graphics.DrawLine(pen, point2, point3);
+                graphics.DrawLine(pen, point3, point1);
+            }
+
+            float angle = (float)(Math.PI / 3.0f);
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            float[,] rotationMatrix = new float[2, 2] { { cos, -sin }, { sin, cos } };
+
+            DrawOuterTriangle(point1, point2, depth);
+            DrawOuterTriangle(point2, point3, depth);
+            DrawOuterTriangle(point3, point1, depth);
+
+            void DrawOuterTriangle(PointF p1, PointF p2, int currentDepth)
+            {
+                if (currentDepth < 1)
+                {
+                    graphics.DrawLine(pen, p1, p2);
+                    return;
+                }
+
+                float dx = (p2.X - p1.X) / 3.0f;
+                float dy = (p2.Y - p1.Y) / 3.0f;
+
+                PointF t1 = new PointF((p2.X + 2.0f * p1.X) / 3.0f, (p2.Y + 2.0f * p1.Y) / 3.0f);
+                PointF t2 = new PointF((2.0f * p2.X + p1.X) / 3.0f, (2.0f * p2.Y + p1.Y) / 3.0f);
+                PointF tV = new PointF(t1.X + rotationMatrix[0, 0] * dx + rotationMatrix[0, 1] * dy,
+                                       t1.Y + rotationMatrix[1, 0] * dx + rotationMatrix[1, 1] * dy);
+
+                DrawOuterTriangle(p1, t1, currentDepth - 1);
+                DrawOuterTriangle(t1, tV, currentDepth - 1);
+                DrawOuterTriangle(tV, t2, currentDepth - 1);
+                DrawOuterTriangle(t2, p2, currentDepth - 1);
+            }
+        }
+    }
+}
